88286308d7de2dcdddf0f3d4c0966a5057ef9c0f
sherrycayheyhey <sherrycaywood@yahoo.com>
2020-05-25 19:35:11 -0700

    added onCompletion toast message

--- app/src/main/java/com/chromsicle/mediaplayersampleapp/MainActivity.java
+++ app/src/main/java/com/chromsicle/mediaplayersampleapp/MainActivity.java
@@ -6,6 +6,9 @@ import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+//cool state machine diagram for the MediaPlayer https://developer.android.com/reference/android/media/MediaPlayer.html
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,7 +26,8 @@ public class MainActivity extends AppCompatActivity {
         Button stopButton = findViewById(R.id.stop_button);
 
         //create the media player by using the song file in the raw directory
-        mediaPlayer = MediaPlayer.create(this, R.raw.tranquility2);
+        mediaPlayer = MediaPlayer.create(this, R.raw.number_one);
+
 
         //set the onClickListeners to do stuff when the buttons are pressed
 
@@ -32,8 +36,19 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 mediaPlayer.start();
+                //when the song is done, show a toast
+                //this uses an asynchronous callback to notify us when the MediaPlayer is done playing the song
+                //in the meantime, we can go about our merry way while waiting to get called back and
+                // do stuff like respond to other user button clicks such as the ones below like pause or seek
+                mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                    @Override
+                    public void onCompletion(MediaPlayer mediaPlayer) {
+                        Toast.makeText(getApplicationContext(), "Song done!", Toast.LENGTH_SHORT).show();
+                    }
+                });
             }
         });
+        //you don't have to call prepare() here because it's already called automatically when the player was created
 
         //seek to a random position in the song
         randomButton.setOnClickListener(new View.OnClickListener() {
@@ -58,7 +73,11 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 mediaPlayer.reset();
+                //once the mediaPlayer is stopped it can't be started again so it would have to be
+                //prepared again to play it again
             }
         });
+
+
     }
 }
